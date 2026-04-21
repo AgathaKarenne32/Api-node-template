8402d44d3a1921b4a7013536eb88f959e6f36d82
Agatha Karenne <agathakarenne32@gmail.com>
2026-04-20 23:16:05 -0300

    feat: setup database schema and environment variables

--- src/envs/index.ts
+++ src/envs/index.ts
@@ -1,7 +1,8 @@
 import "dotenv/config";
 
 export const envs = {
-  PORT: Number(process.env.PORT),
+  PORT: Number(process.env.PORT) || 3030,
   DATABASE_URL: process.env.DATABASE_URL as string,
-  // Add here any additional environment variables you need for your application
+  JWT_SECRET: process.env.JWT_SECRET as string,
+  JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN || "1d",
 };
